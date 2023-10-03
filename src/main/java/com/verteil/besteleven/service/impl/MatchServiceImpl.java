2e65037a39dae98f52437d61f91525e8ccc4407c
VivekNP19 <129286937+VivekNP19@users.noreply.github.com>
2023-10-03 20:13:23 +0530

    Revert "UI changes"

--- src/main/java/com/verteil/besteleven/service/impl/MatchServiceImpl.java
+++ src/main/java/com/verteil/besteleven/service/impl/MatchServiceImpl.java
@@ -44,7 +44,7 @@ public class MatchServiceImpl implements MatchService {
 
     private boolean checkDatePassed(LocalDate matchDate) {
         LocalDateTime timeToCheck = LocalDateTime.now();
-        LocalDateTime matchTime = LocalDateTime.of(matchDate, LocalTime.of(3, 0));
+        LocalDateTime matchTime = LocalDateTime.of(matchDate, LocalTime.of(8, 30));
         return matchTime.isBefore(timeToCheck);
     }
 }
